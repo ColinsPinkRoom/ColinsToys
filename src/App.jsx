750dd09dcb43e0a676556d2011723c79c437ab9f
Christofer Carlsson <113172285+ChristoferCarlsson@users.noreply.github.com>
2025-06-10 21:50:45 +0200

    Added a streaming part
    Also fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,11 @@ import Contact from "./modules/Pages/Contact.jsx";
 import Socials from "./modules/Pages/Socials.jsx";
 import Home from "./modules/Pages/Home.jsx";
 
+import "./style/App.css";
+
 function App() {
   return (
     <div id="App">
-      <h2 id="mainTitle">Toys for Colin</h2>
-
-      <p className="intro-text">
-        Welcome to "Toys for Colin"! This is a curated list of toys and products
-        that I think Colin would enjoy. The list is updated regularly, so check
-        back often for new additions. If you have any suggestions or want to add
-        something, feel free to let me know!
-      </p>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
