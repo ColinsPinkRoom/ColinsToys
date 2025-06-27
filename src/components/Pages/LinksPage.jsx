f3d8411ea3aa6188c68fb3d008dd3a71cab02960
Christofer Carlsson <113172285+ChristoferCarlsson@users.noreply.github.com>
2025-06-27 20:57:12 +0200

    Added the page

--- src/components/Pages/LinksPage.jsx
+++ src/components/Pages/LinksPage.jsx
@@ -2,7 +2,40 @@ import React from "react";
 import "../../style/Links.css";
 
 function LinksPage() {
-  return <div>LinksPage</div>;
+  return (
+    <div className="links-container">
+      {/* 1. Links Section */}
+      <section className="links-link-section">
+        <div className="links-groups">
+          <div className="links-card">
+            <h2 className="links-title">Purchase Content</h2>
+            <a
+              href="https://onlyfans.com/colins_pink_room"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={`${import.meta.env.BASE_URL}/icons/onlyfans.svg`}
+                alt="OnlyFans"
+              />
+              Link 1
+            </a>
+            <a
+              href="https://fansly.com/Colins_Pink_Room/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={`${import.meta.env.BASE_URL}/icons/fansly.svg`}
+                alt="Fansly"
+              />
+              Link 2
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default LinksPage;
