c09ffa7759d9dbb7b24853eb7178faa9dce05b07
Christofer Carlsson <113172285+ChristoferCarlsson@users.noreply.github.com>
2025-07-11 21:41:46 +0200

    Fixed bug where background moved.

--- src/App.jsx
+++ src/App.jsx
@@ -15,33 +15,37 @@ import "./style/App.css";
 
 function App() {
   return (
-    <div id="App">
-      <Link className="logo-container" to="">
-        <div>
-          <img
-            src={`${import.meta.env.BASE_URL}/logos/Logo-website.svg`}
-            alt="Logo"
-          />
-        </div>
-      </Link>
+    <>
+      <div id="background-overlay"></div>
+      <div id="App">
+        <Link className="logo-container" to="">
+          <div>
+            <img
+              src={`${import.meta.env.BASE_URL}/logos/Logo-website.svg`}
+              alt="Logo"
+            />
+          </div>
+        </Link>
 
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ToysPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/links" element={<LinksPage />} />
-        <Route path="/socials" element={<SocialsPage />} />
-        <Route path="/prices" element={<PricesPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      <footer id="footer">
-        <p>
-          &copy; 2025 Colins Pink Room. All rights reserved. Power to the Booty
-        </p>
-      </footer>
-    </div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ToysPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/links" element={<LinksPage />} />
+          <Route path="/socials" element={<SocialsPage />} />
+          <Route path="/prices" element={<PricesPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <footer id="footer">
+          <p>
+            &copy; 2025 Colins Pink Room. All rights reserved. Power to the
+            Booty
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
 
